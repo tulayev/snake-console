777ba619f60022b7f51fefeb0314e141bb063687
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2022-09-08 01:45:08 +0500

    Update Wall.cs

--- Game/Wall.cs
+++ Game/Wall.cs
@@ -2,9 +2,9 @@
 {
     public static class Wall
     {
-        private static int _width = Console.WindowWidth - 10;
+        private static readonly int _width = Console.WindowWidth - 10;
 
-        private static int _height = Console.WindowHeight - 10;
+        private static readonly int _height = Console.WindowHeight - 10;
 
         public static void DrawWall()
         {
@@ -12,10 +12,6 @@
             {
                 Console.SetCursorPosition(i, 0);
                 Console.Write('-');
-            }
-            
-            for (int i = 0; i < _width; i++)
-            {
                 Console.SetCursorPosition(i, _height);
                 Console.Write('-');
             }
@@ -24,10 +20,6 @@
             {
                 Console.SetCursorPosition(0, i);
                 Console.Write('|');
-            }
-            
-            for (int i = 0; i < _height; i++)
-            {
                 Console.SetCursorPosition(_width, i);
                 Console.Write('|');
             }
